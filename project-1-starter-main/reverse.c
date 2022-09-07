4735ff8342c312dcb3487a596e30aa828e2f4f3c
CharlesMowbray <charlesmowbray@mines.edu>
2022-09-06 20:49:17 -0600

    Working version.

--- project-1-starter-main/reverse.c
+++ project-1-starter-main/reverse.c
@@ -1,7 +1,127 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+struct node{
+
+char* line;
+
+struct node* previous;
+struct node* next;
+
+};
+
+
+
+
+
 
 int main(int argc, char *argv[])
 {
-	printf("TODO: write a main function\n");
+
+	if(argc < 2  || argc > 3){
+		fprintf(stderr, "usage: reverse <input> <output>\r\n");
+		exit(1);
+	}
+
+	printf("# of args: %d \nname: %s & %s\n", argc, argv[1], argv[2]);
+	
+	FILE *outputFile;
+	if (argc == 3)
+	{
+		if(strcmp(argv[1], argv[2]) == 0){
+			fprintf(stderr, "error: input and output file must differ\r\n");
+			exit(1);
+		}
+
+		
+
+		if ((outputFile = fopen(argv[2], "w")) == NULL)
+		{
+			fprintf(stderr, "error: cannot open file '%s'\r\n", argv[2]);
+			exit(1);
+		}
+		
+
+	}
+
+
+	FILE *file1;
+	if ((file1 = fopen(argv[1], "r")) == NULL)
+	{
+		fprintf(stderr, "error: cannot open file '%s'\r\n", argv[1]);
+		exit(1);
+	}
+
+
+
+
+	char* buffer= NULL;
+	size_t buffSize = 0;
+	ssize_t lineOut;
+
+
+	struct node* head = malloc(sizeof( struct node));
+	head->next = NULL;
+	head->previous = NULL;
+	head->line = NULL;
+	struct node* curLine = head;
+	struct node* holder = malloc(sizeof( struct node));
+	struct node* tail = malloc(sizeof( struct node));
+	tail->line = NULL;
+
+	 while ((lineOut = getline(&buffer, &buffSize, file1)) != -1){
+
+		holder->next = curLine;
+		curLine->next = malloc(sizeof*(curLine->next));
+
+		curLine = curLine->next;
+		curLine->previous = holder->next;
+
+		curLine->line = strdup(buffer);
+		curLine->next = NULL; 
+		tail->next = curLine;
+
+	}
+	
+if (argc == 2)
+{
+	struct node* cur = tail->next;
+	while(cur->previous != NULL){
+		printf("%s\n", cur->line );
+		cur = cur->previous;
+	}
+}
+else{
+
+
+struct node* cur = tail->next;
+while(cur->previous != NULL){
+	fputs(cur->line, outputFile);
+	cur = cur->previous;
+}
+fclose(outputFile);
+
+}
+
+
+
+	
+	while(head != NULL){
+		struct node* temp = head;
+		head = head->next;
+		free(temp->line);
+		free(temp);
+	}
+
+	free(holder);
+	free(tail);
+	free(buffer);
+	fclose(file1);
+	
+
 	return 0;
 }
